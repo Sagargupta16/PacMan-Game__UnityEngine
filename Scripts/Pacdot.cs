5d44e557806027215482105249548294554ea294
Sagar Gupta <sg85207@gmail.com>
2022-06-01 08:44:19 +0530

    01-06-2022

---
+++ Scripts/Pacdot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacman")
+            Destroy(gameObject);
+    }
+}
